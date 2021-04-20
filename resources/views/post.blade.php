1dda386da3e9356e9deff42b3593c3b2daeff3c4
Ntwari Moise <mozayntwali@gmail.com>
2021-04-20 13:01:25 +0200

    Routes & view pages

---
+++ resources/views/post.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+
+    <body>
+        <article>
+            <h1>My first post</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima corrupti ipsum inventore earum nisi asperiores omnis. Maxime illum blanditiis illo! Quod, temporibus ab architecto illum ut rem debitis recusandae molestias.
+            </p>
+        </article>
+
+        <a href="/">Go back</a>
+    </body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+
+    <body>
+        <article>
+            <h1><a href="/post">My first post</a></h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima corrupti ipsum inventore earum nisi asperiores omnis. Maxime illum blanditiis illo! Quod, temporibus ab architecto illum ut rem debitis recusandae molestias.
+            </p>
+        </article>
+
+        <article>
+            <h1><a href="/post">My second post</a></h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima corrupti ipsum inventore earum nisi asperiores omnis. Maxime illum blanditiis illo! Quod, temporibus ab architecto illum ut rem debitis recusandae molestias.
+            </p>
+        </article>
+
+        <article>
+            <h1><a href="/post">My third post</a></h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima corrupti ipsum inventore earum nisi asperiores omnis. Maxime illum blanditiis illo! Quod, temporibus ab architecto illum ut rem debitis recusandae molestias.
+            </p>
+        </article>
+        
+    </body>
