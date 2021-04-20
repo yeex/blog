59173b550643b37ce5a94c10ac562971a1bef184
Ntwari Moise <mozayntwali@gmail.com>
2021-04-20 15:12:41 +0200

    Cache on posts

--- routes/web.php
+++ routes/web.php
@@ -18,13 +18,14 @@ Route::get('/', function () {
 });
 
 Route::get('posts/{post}', function ($slug) {
-    $path = __DIR__ . "/../resources/posts/{$slug}.html";
 
-    if(!file_exists($path)) {
+    if(!file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
         return redirect('/');
     }
 
-    $post = file_get_contents($path);
+    $post = cache()->remember("posts.{$slug}", now()->addMinutes(), function () use ($path) {
+        return file_get_contents($path);
+    });
 
     return view('post', [
         'post' => $post
